c3d1d0687adca3af689324e2710cacf03d3cc770
billturechek <30839042+billturechek@users.noreply.github.com>
2024-02-02 15:24:00 -0500

    Change for Godaddy

---
+++ wwwroot/js/togglePassword.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    const showIcon = document.querySelector("#hideShow");
+    const passwordField = document.querySelector("#password-field");
+
+    showIcon.addEventListener("click", function () {
+        this.classList.toggle("fa-eye-slash");
+        const type = passwordField.getAttribute("type")
+            === "password" ? "text" : "password";
+        passwordField.setAttribute("type", type);
+
+        this.classList.toggle("fa-eye");        
+    })
+});
